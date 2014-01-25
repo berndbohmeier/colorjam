58727261e5f7cdc33dff537590079d4414ff99df
ahhdbhebebj <ahhdbhebebj@googlemail.com>
2014-01-25 11:49:53 +0000

    Created EnitityFramework and LevelParser

--- colorjam/web/src/entity_factory.dart
+++ colorjam/web/src/entity_factory.dart
@@ -1,8 +1,19 @@
 part of colorjam;
 
-abstract class EntityFactory {
-  static Map<String, Entity> _factories = Map<String, Entity>();
-  factory EntityFactory(String name) {
+class EntityFactory {
+  static final Map<String, EntityFactory> _factories = {
+    "Entity" : new EntityFactory._()
+  };
+  
+  EntityFactory._() {}
+  
+  factory EntityFactory.forType(String entityType) {
+    if(_factories.containsKey(entityType))
+      return _factories[entityType];
     
+    throw new ArgumentError("No factory for ${entityType} found.");
   }
+  
+  Entity build(World world, Map<String, dynamic> args)
+    => world.createEntity();
 }
