d1228303fc31b52027de905f90da859942cdfa52
berndbohmeier <berndbohmeier@googlemail.com>
2014-01-25 16:28:14 +0000

    szenes

--- colorjam/web/src/szenes/level_szene.dart
+++ colorjam/web/src/szenes/level_szene.dart
@@ -1,1 +1,17 @@
 part of colorjam;
+
+
+
+
+
+class LevelSzene extends WorldSzene{
+  
+  LevelSzene(String level, World world, DisplayObjectContainer headcontainer) : super(world, headcontainer);
+  
+  
+  void init(){
+    
+  }
+  
+  
+}
