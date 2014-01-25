1afcf0e6f319eabaed6eb49b3f32beb0e656cd84
mail@simon-weiler.de <mail@simon-weiler.de@02dae8fd-3d38-4a42-5bf5-f071bf32de66>
2014-01-25 14:38:38 +0000

    Wall Factory is now finished

--- colorjam/web/src/factories/wall_factory.dart
+++ colorjam/web/src/factories/wall_factory.dart
@@ -1,6 +1,32 @@
 part of colorjam;
 
 class WallFactory extends EntityFactory {
+  static const String ARG_TOP = "top";
+  static const String ARG_LEFT = "left";
+  static const String ARG_RIGHT = "right";
+  static const String ARG_BOTTOM = "bottom";
+  static const String ARG_BOUNCINESS = "bounciness";
+
   
   WallFactory._() : super._();
+  
+  Entity build(World world, Map<String, dynamic> args) {
+    ColorComponent colorComp = new ColorComponent.fromJson(args);
+    num top = args[ARG_TOP] as num;
+    num left = args[ARG_LEFT] as num;
+    num right = args[ARG_RIGHT] as num;
+    num bottom = args[ARG_BOTTOM] as num;
+    Sprite sprite = new Sprite();
+    sprite.graphics.rect(0,0,right - left, bottom - top);
+    sprite.graphics.fillColor(colorComp.colorcode);
+    
+    Entity entity = world.createEntity()
+        ..addComponent(new PositionComponent(left + (right-left)/2, top + (bottom-top)/2))
+        ..addComponent(new VelocityComponent(0,0))
+        ..addComponent(new SpriteComponent(sprite))
+        ..addComponent(new ColliderComponent(bounciness: args[ARG_BOUNCINESS]))
+        ..addComponent(new GeometryComponent(right - left, bottom - top))
+        ..addToWorld();
+    return entity;
+  }
 }
