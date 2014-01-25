fd22dea02e1951d2aa9a990bca768e2793f2389e
ahhdbhebebj <ahhdbhebebj@googlemail.com>
2014-01-25 12:09:20 +0000

    Added comments to EntityFactory and LevelParser.

--- colorjam/web/src/level_parser.dart
+++ colorjam/web/src/level_parser.dart
@@ -4,6 +4,10 @@ class LevelParser {
   World world;
   LevelParser(this.world);
   
+  /**
+   * Create entities from a json object and
+   * add them to the world.
+   */
   void parse(String json) {
     Map data = JSON.decode(json);
     new EntityFactory.forType(data["type"])
