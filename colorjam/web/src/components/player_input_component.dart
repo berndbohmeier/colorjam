7bbc8e26d1b26f23e7cf09c1c350bbc8dff33935
berndbohmeier@googlemail.com <berndbohmeier@googlemail.com@02dae8fd-3d38-4a42-5bf5-f071bf32de66>
2014-01-25 12:58:57 +0000

    control systems started

---
+++ colorjam/web/src/components/player_input_component.dart
@@ -0,0 +1,12 @@
+part of colorjam;
+
+
+class PlayerInputComponent extends Component{
+  
+  bool leftKeyPressed = false;
+  bool rightKeyPressed = false;
+  bool upKeyPressed = false;
+  
+  
+  
+}
