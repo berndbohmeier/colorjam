e26816bf74c6db3dbe4c69e170450911ff669e6d
berndbohmeier <berndbohmeier@googlemail.com>
2014-01-25 14:09:45 +0000

    fix player factory

--- colorjam/web/src/factories/player_factory.dart
+++ colorjam/web/src/factories/player_factory.dart
@@ -18,7 +18,7 @@ class PlayerFactory extends EntityFactory {
         ..addComponent(new PhysicsComponent())
         ..addComponent(new GeometryComponent(30, 30))
         ..addComponent(new PlayerControlComponent())
-        ..addComponent(new PlayerInputComponent(html.KeyCode.LEFT, html.KeyCode.RIGHT, html.KeyCode.UP));
+        ..addComponent(new PlayerInputComponent(html.KeyCode.LEFT, html.KeyCode.RIGHT, html.KeyCode.UP))
         ..addToWorld();
     
     return entity;
