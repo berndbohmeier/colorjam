8c2da6502afd0397bdadaaefeec72721339f6db0
mail@simon-weiler.de <mail@simon-weiler.de@02dae8fd-3d38-4a42-5bf5-f071bf32de66>
2014-01-25 13:21:05 +0000

    VelocityComponent has now setter

--- colorjam/web/src/components/velocity_component.dart
+++ colorjam/web/src/components/velocity_component.dart
@@ -14,4 +14,11 @@ class VelocityComponent extends Component {
   num get vx => velocity.x;
   num get vy => velocity.y;
   
+  void set vx (num nvx) {
+    velocity = new Vector(nvx, vy);
+  }
+  
+  void set vy (num nvy) {
+    velocity = new Vector(vx, nvy);
+  }
 }
