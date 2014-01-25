a4f112c7c1dedc54922a4f6b164250404fcf4187
berndbohmeier@googlemail.com <berndbohmeier@googlemail.com@02dae8fd-3d38-4a42-5bf5-f071bf32de66>
2014-01-25 13:52:06 +0000

    playercontrol update

--- colorjam/web/src/systems/player_control_system.dart
+++ colorjam/web/src/systems/player_control_system.dart
@@ -1,1 +1,39 @@
 part of colorjam;
+
+
+class PlayerControlSystem extends EntityProcessingSystem{
+  
+  
+  PlayerControlSystem() : super(Aspect.getAspectForAllOf([PlayerControlComponent, PlayerInputComponent]));
+  
+  ComponentMapper<PlayerControlComponent> controlMapper;
+  ComponentMapper<PlayerInputComponent> inputMapper; 
+  
+  
+  void processEntity(Entity entity){
+    PlayerControlComponent control = controlMapper.get(entity);
+    PlayerInputComponent input = inputMapper.get(entity);
+    
+    if(input.leftKeyPressed){
+      control.direction = -1;
+    }else{
+      if(input.rightKeyPressed){
+        control.direction = 1;
+      }else{
+        control.direction = 0;
+      }
+    }
+    
+    if(input.jumpKeyPressed){
+      control.doJump();
+    }
+    
+    input.leftKeyPressed = false;
+    input.rightKeyPressed = false;
+    input.jumpKeyPressed = false;
+    
+  }
+  
+  
+  
+}
