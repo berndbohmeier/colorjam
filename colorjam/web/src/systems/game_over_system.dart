50d75e4c02cb6a6060a6142c8c487a30e36c43ad
ahhdbhebebj <ahhdbhebebj@googlemail.com>
2014-01-26 13:11:15 +0000

    GameOverScene is displayed when player dies. Layout needs improvement.

--- colorjam/web/src/systems/game_over_system.dart
+++ colorjam/web/src/systems/game_over_system.dart
@@ -2,7 +2,7 @@ part of colorjam;
 
 typedef void GameOverCallback();
 class GameOverSystem extends EntityProcessingSystem {
-  int _count = 0;
+  //int _count = 0;
   GameOverCallback onGameOver;
   ComponentMapper<ColorComponent> colorMapper;
   GameOverSystem(this.onGameOver) : super(Aspect.getAspectForAllOf([PlayerComponent, ColorComponent]));
@@ -13,17 +13,21 @@ class GameOverSystem extends EntityProcessingSystem {
   }
   
   void processEntity(Entity e) {
-    if((colorMapper.get(e).colorcode & 0xFFFFFF) == 0)
+    if((colorMapper.get(e).colorcode & 0xFFFFFF) == 0) {
       e.deleteFromWorld();
+    }
   }
-  
+  /*
   void added(Entity e) {
     _count++;
     super.added(e);
-  }
+  }*/
+  
   void removed(Entity e) {
-    super.removed(e);
-    if(--_count == 0)
+    super.removed(e);/*
+    print("Game Over");
+    print("removed $e, ${_count-1} players");
+    if(--_count == 0)*/
       onGameOver();
   }
 }
