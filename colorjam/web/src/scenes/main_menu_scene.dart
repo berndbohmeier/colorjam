5eb7017bf463b7a48ca90ad1e71d30528eb5d9a4
mail@simon-weiler.de <mail@simon-weiler.de@02dae8fd-3d38-4a42-5bf5-f071bf32de66>
2014-01-26 15:16:06 +0000

    Logo added

--- colorjam/web/src/scenes/main_menu_scene.dart
+++ colorjam/web/src/scenes/main_menu_scene.dart
@@ -4,15 +4,21 @@ class MainMenuScene extends Scene {
   MainMenuScene(Game game, DisplayObjectContainer obj) : super(game, obj);
   Button playButton =   new Button("       Play");
   Button editorButton = new Button("Level Editor");
+  Sprite logo = new Sprite();
   
   void init() {
     editorButton.y = 30;
+    logo.addChild(new Bitmap(resourceManager.getBitmapData("logo")));
+    
     
     playButton.onMouseClick.listen((MouseEvent e) { game.restartLevel(); });
     editorButton.onMouseClick.listen((MouseEvent e) { game.loadEditor(); });
     
+    logo.x = -120;
+    logo.y = -120;
     container.addChild(playButton);
     container.addChild(editorButton);
+    container.addChild(logo);
     
     container.x = 400 - container.width / 2;
     container.y = 300 - container.height / 2;
