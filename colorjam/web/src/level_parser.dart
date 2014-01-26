b9087751e924cb5f9c60ab3ef95757ebff0fd9d9
berndbohmeier <berndbohmeier@googlemail.com>
2014-01-26 13:19:05 +0000

    dfdf

--- colorjam/web/src/level_parser.dart
+++ colorjam/web/src/level_parser.dart
@@ -9,11 +9,21 @@ class LevelParser {
    * add them to the world.
    */
   void parse(String json) {
+    try{
+    
     List<Map<String, dynamic>> entities = JSON.decode(json)["entities"];
     for(Map<String, dynamic> data in entities) {
       new EntityFactory.forType(data["type"])
         .build(world, data)
         .addToWorld();
+      }
+    }on FormatException catch(e){
+      print("${e.message}");
+      html.window.alert("${e.message}");
+    }on ArgumentError catch( e) {
+      print("${e.message}");
+      html.window.alert("${e.message}");
     }
+    
   }
 }
