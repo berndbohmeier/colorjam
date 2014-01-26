a10bf8ecd54d8a91b0040d18a260dc6655643950
berndbohmeier@googlemail.com <berndbohmeier@googlemail.com@02dae8fd-3d38-4a42-5bf5-f071bf32de66>
2014-01-26 11:02:07 +0000

    goal

--- colorjam/web/src/factories/goal_factory.dart
+++ colorjam/web/src/factories/goal_factory.dart
@@ -1,19 +1,23 @@
 part of colorjam;
 
 class GoalFactory extends EntityFactory {
-  static const WIDTH = 50, HEIGHT = 50;
+  static const WIDTH = 60, HEIGHT = 60;
   GoalFactory._() : super._() {}
   
   Entity build(World world, Map<String, dynamic> args) {
     Sprite sprite = new Sprite();
-    sprite.graphics.rect(0,0,WIDTH, HEIGHT);
-    sprite.graphics.strokeColor(0x55FFFFFF, 3);
-    sprite.graphics.fillColor(0xFFFFFFFF);
+
+    Bitmap bitmap = new Bitmap(resourceManager.getBitmapData("goal"));
+    sprite.addChild(bitmap);
+    sprite.width = bitmap.width.round();
+    sprite.height = bitmap.height.round();
     
+    ColorComponent cp = new ColorComponent.fromJson(args);
+    cp.alpha = false;
     Entity e = super.build(world, args)
       ..addComponent(new TypeComponent("Goal"))
       ..addComponent(new PositionComponent.fromJson(args))
-      ..addComponent(new ColorComponent.fromJson(args))
+      ..addComponent(cp)
       ..addComponent(new GoalComponent())
       ..addComponent(new GeometryComponent(WIDTH, HEIGHT))
       ..addComponent(new SpriteComponent(sprite));
